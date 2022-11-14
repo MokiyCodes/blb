50b468516b2a53c512aa14cdd8eb4d8851afe19e
MokiyCodes <mokiycodes@users.noreply.github.com>
2022-11-14 23:59:20 +0100

    feat: :sparkles: add ccsh command

--- dev.js
+++ dev.js
@@ -11,6 +11,8 @@ app.listen(16969, () => console.log(`> Listening on port 16969 <
 Load Using:
 - Roblox Executor:
     loadstring(game:HttpGetAsync'http://127.0.0.1:16969/build.lua','devbuild.rbx')(); ${/* as provided by yielding's code */ ''}
-- ComputerCraft:
+- ComputerCraft Lua:
     (function(rq)loadstring(rq.readAll(),'devbuild-wrapper')();rq.close();end)(http.get('https://github.com/MokiyCodes/blb/raw/main/.cc-devloader.lua'));
-    --> requires changing config/computercraft.cfg to allow 127.0.0.0`));
+    --> requires changing config/computercraft.cfg to allow 127.0.0.0
+- ComputerCraft Shell:
+    wget run http://127.0.0.1:16969/build.lua`));
